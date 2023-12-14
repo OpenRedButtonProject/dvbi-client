5d154b62de77a230a3c59b44d7645625ee526e7b
Marios Christoforakis <marios.christoforakis@oceanbluesoftware.co.uk>
2023-12-14 12:23:46 +0200

    HTM-735: Embed dvbipage.html within the output apk.

--- src/main/Android.mk
+++ src/main/Android.mk
@@ -9,6 +9,9 @@ LOCAL_STATIC_JAVA_LIBRARIES += \
 
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
 
+ASSETS_PATH = "$(PRODUCT_OUT)/org.orbtv.polyfill/resources"
+IGNORED := $(shell mkdir -p $(ASSETS_PATH) && cp -r $(LOCAL_PATH)/assets $(ASSETS_PATH))
+
 include $(BUILD_STATIC_JAVA_LIBRARY)
 include $(call all-makefiles-under, $(LOCAL_PATH))
 
